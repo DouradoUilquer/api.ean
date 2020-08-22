5e9c88e7893b888b720f19715b29cc44dcfb9fdd
DouradoUilquer <69002166+DouradoUilquer@users.noreply.github.com>
2020-08-22 14:45:06 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
     client: 'mysql2',
     connection: {
-      host : '93.188.166.54',
-      user : 'root',
-      password : 'connectdelivery18',
-      database : 'connect_api'
+      host : '#',
+      user : '#',
+      password : '#',
+      database : '#'
     }
   });
 
