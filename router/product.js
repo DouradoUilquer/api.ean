bdbe8a3907976b9b0b665b7fd024833f3f707f36
DouradoUilquer <69002166+DouradoUilquer@users.noreply.github.com>
2020-08-22 14:47:12 -0300

    Update product.js

--- router/product.js
+++ router/product.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const dataBase = require("../database/database");
 const adminAuth = require("../middleware/authToken");
 
-router.get("/product/:ean",(req, res) => {
+router.get("/product/:ean",adminAuth,(req, res) => {
     var ean = req.params.ean;
     dataBase.select('ean','description','ncm','cest').table("product").where('ean',ean).then(product => {
         res.json({ product })
